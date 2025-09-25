8ac6a7a139269a3b103fca8779a7793a67d459e5
Tarun <thakurtarun1012@gmail.com>
2025-09-25 20:05:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+   let str1 = s1.toLowerCase();
+let arr = str1.split(" ");
+let ss = s2.toLowerCase();
+	let arr2 = ss.split(" ");
+let len = 0;
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] == arr2[0]) break;
+    len += arr[i].length + i;;
+}
+return len;
+
 }
 
-// Please do not change the code below
+Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
+
 alert(indexOfIgnoreCase(s1, s2));
