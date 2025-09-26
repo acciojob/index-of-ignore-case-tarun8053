92520d7f6de2d93541a77819668699beef86c43f
Tarun <thakurtarun1012@gmail.com>
2025-09-26 13:17:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function indexOfIgnoreCase(s1, s2) {
 	
 }
 
-Please do not change the code below
+//Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 
